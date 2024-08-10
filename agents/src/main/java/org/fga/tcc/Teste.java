1cf5483c3e8c55900dc323ee6f52fd07c1bf8dce
DouglasMonteles <silvadouglas328@gmail.com>
2024-08-10 17:43:21 -0300

    :heavy_plus_sign: Add sub-modules core, agents and traiened-lstm

---
+++ agents/src/main/java/org/fga/tcc/Teste.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Teste {
+
+    public void teste() {
+        System.out.println("teste");
+    }
+
+}
--- core/src/test/java/org/example/AppTest.java
+++ core/src/test/java/org/example/AppTest.java
@@ -1,4 +1,4 @@
-package org.fga.tcc;
+package org.example;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
